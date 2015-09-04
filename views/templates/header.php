a3e9fa5fa86b91757306b2ff227b4e945e3c3ce7
ilynehan@umail.ucsb.edu <ilynehan@gmail.com>
2015-09-03 22:20:36 -0700

    working redirect using .htaccess and printed Contact Info

---
+++ views/templates/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width">
+        <title><?php echo 'Three Aces'  ?></title>
+    </head>
+    <body>
+        <h1><?php echo 'Three Aces' ?></h1>
+        <h2><?php echo htmlspecialchars($title) ?></h2>
